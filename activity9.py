2023bed15d15dbe5e2aa9eb70670c83d8bcb5453
kccirilo1B <kccirilo09@gmail.com>
2025-08-18 15:48:03 +0800

    commit activity9.py

---
+++ activity9.py
@@ -0,0 +1,11 @@
+a = 6
+b = 4
+
+c = 7
+d = 6
+username = 'kcirilo'
+password = 'kcamille'
+print((username == 'kcirilo' ) and (password == 'kcamille'))
+#print( (a > b) and (c < d) )
+#print( (a > b) or (c < d) )
+#print(not((a > b) or (c < d) ))
