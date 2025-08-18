e8dbede6ac72f89b4e4fee1a28108063caf649ee
kccirilo1B <kccirilo09@gmail.com>
2025-08-18 15:47:05 +0800

    commit activity7.py

---
+++ activity7.py
@@ -0,0 +1,19 @@
+a = 9
+
+print("The value of a is", a)
+
+a += 30
+
+print("The value of a is", a)
+
+a *= 14
+a -= 2
+
+print("The value of a is", a)
+
+a += 7
+a -= 8
+a *= 9
+a /= 10
+
+print("The value of a is", a)
