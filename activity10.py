db049e49a6b0c92177eaf070cdda577eed99493d
kccirilo1B <kccirilo09@gmail.com>
2025-08-27 15:08:24 +0800

    commit activity10.py

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "camille_user"
+password = "pogika123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+        print("Login Successful")
+else:
+        print("Login Unsuccessful")
