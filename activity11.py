90820cf10641ee3c52c1768d05b195eb928b02bc
kccirilo1B <kccirilo09@gmail.com>
2025-09-15 15:10:09 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,28 @@
+temp_out = eval(input("Temperature? --> "))
+
+if temp_out >=1 and temp_out <=15 :
+	print("Extremely Cold")
+
+if temp_out >=16 and temp_out <=30 :
+	print("Cold")
+
+if temp_out >=31 and temp_out <=38 :
+	print("Lukewarm")
+
+if temp_out >=39 and temp_out <=42 :
+	print("Warm")
+
+if temp_out >=43 and temp_out <=50 :
+	print("Hot")
+
+if temp_out >=51 and temp_out <=60 :
+	print("Extremely Hot")
+
+if temp_out >=61 and temp_out >61 :
+	print("Burning")
+
+
+
+
+
+
